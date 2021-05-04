a25f6d6f89cbde1b498fad1339efcff45687c22a
DESKTOP-05QA44M\user <jelleeee@naver.com>
2021-05-04 15:39:52 +0900

    refactor

--- src/main/java/edu/axboot/domain/education/EducationTeachGridRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.axboot.domain.education;
-
-import com.chequer.axboot.core.domain.base.AXBootJPAQueryDSLRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EducationTeachGridRepository extends AXBootJPAQueryDSLRepository<EducationTeachGrid, Long> {
-}
